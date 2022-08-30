f441a4ba27450ced1e29a1b99afc7708d468b5ed
dniald <mdanialdariff97@gmail.com>
2022-08-30 13:09:15 +0800

    update policy name

--- .aws-sam/build/getByIdFunction/index.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Updated Through Github Action 😁'),
-    };
-    return response;
- };
